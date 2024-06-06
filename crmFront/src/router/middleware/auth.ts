8b804ee7aee497cf7db96addcbfddc4cbd6d6480
eredikt <vinokurov.vick@yandex.ru>
2024-06-06 17:43:35 +0400

    Update auth.ts

--- crmFront/src/router/middleware/auth.ts
+++ crmFront/src/router/middleware/auth.ts
@@ -1,10 +1,5 @@
 //@ts-ignore
 export function isAuth(to:any, from:any, next: any): void {
-    if (to.name.split('_')[0] === 'crm' && JSON.parse(localStorage.getItem('auth') as string)?.user?.status) {
-        return next()
-    } else if (to.name.split('_')[0] !== 'crm' && JSON.parse(localStorage.getItem('auth') as string)?.user?.status) {
-        return  next({name: 'crm_goods'});
-    } else {
-        next()
-    }
+    if (to.name.split('_')[0] !== 'crm' && JSON.parse(localStorage.getItem('auth') as string)?.user?.status) return next({name: 'crm_goods'});
+    return next();
 }
